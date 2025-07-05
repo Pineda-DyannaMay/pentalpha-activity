350aed23cf41cdec8e46c1c6210282bb0670a538
Pineda Dyanna May <annaydmaypineda15@gmail.com>
2025-07-05 14:48:26 +0800

    Add Pineda - 3

---
+++ app.py
@@ -0,0 +1,11 @@
+# PINEDA: 3 Points
+from flask import Flask, request, jsonify
+from models import view_notes, delete_note
+app = Flask(__name__)
+
+@app.route('/notes', methods=['GET'])
+def get_note():
+    return jsonify(view_notes())
+
+app.run()
+
